c5e05b0e1a0ee161a24d540ee1cc6fc4ea50a26e
Oksana Severovostokova <ksjuha-21@mail.ru>
2017-05-22 13:54:15 +0300

    description entities2

--- src/main/java/shop/models/entity/Share.java
+++ src/main/java/shop/models/entity/Share.java
@@ -38,11 +38,51 @@ public class Share {
     public Share(String title) {
         this.title = title;
     }
-
+    
     public Share(String title, String description, Date startDate, Date endDate) {
         this.title = title;
         this.description = description;
         this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
         this.endDate = endDate;
     }
 }
